c5ee8b3663336af19b4c306d45727f5f151dd419
LeeHyeonha <lej5769@gmail.com>
2025-05-20 15:46:48 +0900

    Feat: user sign up

--- src/main/java/com/sparta/scheduleJPA/controller/UserController.java
+++ src/main/java/com/sparta/scheduleJPA/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sparta.scheduleJPA.controller;
 import com.sparta.scheduleJPA.dto.SignUpRequestDto;
 import com.sparta.scheduleJPA.dto.SignUpResponseDto;
 import com.sparta.scheduleJPA.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +21,18 @@ public class UserController {
 
     /**
      * 회원가입
-     * @param requestDto(email, password)
+     *
+     * @param requestDto
+     * { email, password }
+     *
      * @return
+     * ResponseEntity
      */
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto requestDto){
 
-        SignUpResponseDto signUpResponseDto = userService.signUp(requestDto.getEmail(), requestDto.getPassword());
+        SignUpResponseDto signUpResponseDto = userService.signUp(
+                requestDto.getEmail(), requestDto.getPassword());
+        return new ResponseEntity<>(signUpResponseDto, HttpStatus.CREATED);
     }
 }
