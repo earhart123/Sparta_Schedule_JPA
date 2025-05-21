a4eae0e24d1ace7c08e8c5673f726a3e33a63b2c
LeeHyeonha <lej5769@gmail.com>
2025-05-21 14:22:30 +0900

    Feat: login filter init

---
+++ src/main/java/com/sparta/scheduleJPA/filter/LoginFilter.java
@@ -0,0 +1,38 @@
+package com.sparta.scheduleJPA.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+    private static final String[] WHITE_LIST = {"/", "/signup", "/login"};
+
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        // whitelist에 포함되지 않으면 조건문 실행
+        if(!isWhiteList(requestURI)){
+            HttpSession session = httpRequest.getSession(false);
+
+            if(session==null || session.getAttribute("sessionKey") == null){
+                throw new RuntimeException("로그인 해주세요.");
+            }
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+    private boolean isWhiteList(String requestURI){
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+}
